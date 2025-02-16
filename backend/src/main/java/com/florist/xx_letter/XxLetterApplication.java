21568b3c0a833cea98915d5d7cd05112609430d8
Van Nguyen <87792873+yourvivian@users.noreply.github.com>
2025-02-16 13:26:22 -0800

    Setup codebase

---
+++ backend/src/main/java/com/florist/xx_letter/XxLetterApplication.java
@@ -0,0 +1,13 @@
+package com.florist.xx_letter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxLetterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/florist/xx_letter/XxLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florist.xx_letter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
